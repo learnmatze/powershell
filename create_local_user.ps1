3ff902e5c0591e96af6b803ce3f0fea53b45ee92
learnmatze <138430406+learnmatze@users.noreply.github.com>
2024-01-30 15:02:10 +0100

    initial commit

---
+++ create_local_user.ps1
@@ -0,0 +1,25 @@
+﻿
+<# 
+    ******************************************************
+    Script mit parametern um lokalen Benutzer zu erstellen
+    ******************************************************
+#>
+
+param(
+    [string]$BenutzerName,
+    [string]$Description,
+    [string]$Fullname,
+    [string]$Password
+)
+
+#"12AndiManfred34!"
+
+$secure_password = ConvertTo-SecureString $Password -AsPlainText -Force -Verbose
+
+New-LocalUser -Name $BenutzerName -FullName $Fullname -Description $Description -Password $secure_password -Verbose
+
+# Password $secure_password
+
+# net user $Name /logonpasswordchg:yes
+
+Write-Host "Benutzer, $Name mit $Fullname und Beschreibung $Description wurde erstellt."
